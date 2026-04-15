740a8997266a41e64c390a2636b720aedd3a742e
Clay Morton <lyonsclay@yahoo.com>
2026-04-15 12:47:44 -0400

    Update custom

--- custom.el
+++ custom.el
@@ -3,16 +3,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(compat eglot eldoc jsonrpc track-changes))
- '(projectile-project-root-files
-   '("Cargo.toml" "setup.py" "requirements.txt" "pyproject.toml" "package.json"
-     "__pycache__"))
- '(safe-local-variable-values
-   '((eval and (fboundp 'toc-org-mode) (toc-org-mode 1)) (toc-org-max-depth . 4))))
+ '(package-selected-packages '(compat eca eldoc jsonrpc track-changes)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-document-title ((t (:inherit default :weight bold :font "Lucida Grande" :height 2.0 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :font "Lucida Grande" :height 1.75))))
+ '(org-level-2 ((t (:inherit default :weight bold :font "Lucida Grande" :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :font "Lucida Grande" :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :font "Lucida Grande" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :font "Lucida Grande"))))
+ '(org-level-6 ((t (:inherit default :weight bold :font "Lucida Grande"))))
+ '(org-level-7 ((t (:inherit default :weight bold :font "Lucida Grande"))))
+ '(org-level-8 ((t (:inherit default :weight bold :font "Lucida Grande")))))
